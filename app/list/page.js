6ae3395b2b9fdf0b7ec0999c18054f0b3eafe61a
juncheolkim <kimjoon889@gmail.com>
2023-05-02 19:12:14 +0900

    feat : 상세페이지 만들기1
    익명 함수를 사용할 때, 중괄호 안에 실행할 코드가 한 줄이면 중괄호를 생략해도 된다.
    jsx에서 map함수 안에 익명함수를 사용할 때 return 문 밖에 없을 때, 중괄호와 return을 동시에 제거할 수 있다.
    
    1. 글 제목 누르면 상세페이지 이동
    Link 태그를 사용하여 이동시키면 된다.
    map 안에서 동적으로 생성되는 시점에 Link 태그의 href={`/detail/${a._id}`} 설정해서 경로를 설정
    
    2. db에서 게시물의 정보 보여주기
    url에서 확인된 ObjectId값을 가지는 게시물 정보 출력
    
    url 디자인 어떻게???
    /detail/글번호
    그러면 게시물 갯수만큼 폴더와 파일 만들어야 하나?
    dynamic route 사용하면 비슷한 페이지 여러개 만들 필요 없다.
    해당 url 폴더 하위에 [작명] 형태의 이름을 가진 폴더를 생성하고, 해당 폴더 하위에 page.js 생성.
    그러면 어떤 /detail/글번호 글번호 url이러도 방금 생성한 page.js로 접속한다.
    page.js에서 접근 시 전달받는 props를 확인하면 [작명] 을 key로 가지고, 하위 url을 value로 받는다는 사실을 볼 수 있다. ex. { params: { id: 'aaa' }, searchParams: {} }

--- app/list/page.js
+++ app/list/page.js
@@ -1,20 +1,20 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function list() {
     const db = (await connectDB).db("forum");
     let result = await db.collection("post").find().toArray();
-    console.log(result);
 
     return (
         <div className="list-bg">
-            {result.map((a, i) => {
-                return (
-                    <div className="list-item">
+            {result.map((a, i) => (
+                <Link href={`/detail/${a._id}`}>
+                    <div className="list-item" key={i}>
                         <h4>{result[i].title}</h4>
                         <p>{result[i].content}</p>
                     </div>
-                );
-            })}
+                </Link>
+            ))}
         </div>
     );
 }
